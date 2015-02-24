32d8c027b788093ab33f08b7074f2d6cfcb66599
Nicola Justus <nicola.justus@mni.thm.de>
2015-02-24 22:23:51 +0100

    Added lazily-generation of the local filesystem view.

--- FtpClient/src/ftp/ui/ViewFactory.scala
+++ FtpClient/src/ftp/ui/ViewFactory.scala
@@ -1,17 +1,23 @@
 package ftp.ui
 
-import javafx.scene.control.TreeItem
-import java.io.File
-import javafx.scene.control.TreeView
-import javafx.scene.control.CheckBoxTreeItem
-import javafx.scene.control.cell.CheckBoxTreeCell
-import ftp.ui.filewalker.GenerateTree
-import java.nio.file.Path
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
-import scala.collection.JavaConversions._
+import javafx.beans.value.ChangeListener
+import javafx.beans.value.ObservableValue
+import javafx.scene.control.CheckBoxTreeItem
+import scala.collection.JavaConversions.iterableAsScalaIterable
+import ftp.ui.filewalker.GenerateTree
+import javafx.scene.control.TreeItem
+import javafx.event.EventHandler
+import javafx.event.ActionEvent
+import javafx.scene.control.CheckBoxTreeItem.TreeModificationEvent
+import javafx.beans.property.BooleanProperty
 
 object ViewFactory {
+
+  //dummy-path for recognizing not-generated subfolders
+  private val dummyPath: CheckBoxTreeItem[Path] = new CheckBoxTreeItem[Path](Paths.get("."))
 
   /**
    * Generates a new TreeView from the given file.
@@ -27,14 +33,31 @@ object ViewFactory {
 
   /**
    * Generates a temporary view, without any sub-elements. They needs to lazily generated.
-   *
+   * @param file the (actual) rootpath
    */
   def newLazyView(file: Path): CheckBoxTreeItem[Path] = {
     val root = new CheckBoxTreeItem[Path](file)
+    val listener: ChangeListener[java.lang.Boolean] = new ItemChangeListener()
+
+    //! this is absolutely ugly ! (thanks to the generics)
+    def generateItem(x: Path): CheckBoxTreeItem[Path] = {
+      val item = new CheckBoxTreeItem[Path](x)
+
+      item.expandedProperty().addListener(listener)
+      return item
+    }
 
     //get all entrys without hiddenfiles
     Files.newDirectoryStream(file).filterNot { x => Files.isHidden(x) }.
-      foreach { x => root.getChildren.add(new CheckBoxTreeItem[Path](x)) }
+      foreach { x =>
+        if (Files.isDirectory(x)) {
+          //Add a dummy-children for identifying later in the lazy generation
+          val xItem = generateItem(x)
+          xItem.getChildren.add(dummyPath)
+          root.getChildren.add(xItem)
+        } else
+          root.getChildren.add(new CheckBoxTreeItem[Path](x))
+      }
 
     return root
   }
@@ -55,4 +78,28 @@ object ViewFactory {
     return root
   }
 
+  private class ItemChangeListener extends ChangeListener[java.lang.Boolean] {
+    override def changed(obVal: ObservableValue[_ <: java.lang.Boolean], oldVal: java.lang.Boolean, newVal: java.lang.Boolean): Unit = {
+      /*
+       * newVal = new state of the component (true if expanded, false otherwise)
+       * obVal = the "observed" item
+       * bb.getBean holds the actual Item
+       */
+      if (newVal) {
+        //System.out.println("newValue = " + newVal);
+        val bb = obVal.asInstanceOf[BooleanProperty];
+        //System.out.println("bb.getBean() = " + bb.getBean());
+        val t = bb.getBean.asInstanceOf[TreeItem[Path]];
+        val path = t.getValue
+
+        //set new subpath for the given directory if it's not created yet
+        if (t.getChildren.contains(dummyPath)) {
+          //remove the dummy and replace the childrens
+          t.getChildren.remove(dummyPath)
+          val subview = newView(path)
+          t.getChildren.addAll(subview.getChildren)
+        }
+      }
+    }
+  } //class ItemChangeListener
 }
