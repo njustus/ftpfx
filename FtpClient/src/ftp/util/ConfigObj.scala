ee03dae345291493b25ab1f490acda06f9778e46
Nicola Justus <nicola.justus@mni.thm.de>
2015-03-25 18:01:39 +0100

    Added test-class and implemented configuration loading.

--- FtpClient/src/ftp/util/ConfigObj.scala
+++ FtpClient/src/ftp/util/ConfigObj.scala
@@ -3,10 +3,14 @@ package ftp.util
 import java.nio.file.Paths
 import java.nio.file.Files
 
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+
 object ConfigObj {
-  private val defaultDescription = "ftpfx's default configuration file."
-  private val configPath = Paths.get(getClass.getResource("rsc/conf/ftpfxDefault.conf").toURI())
-  private val langPath = Paths.get(getClass.getResource("rsc/lang/ftpfx-en.conf").toURI())
+  private val defaultConfDescription = "ftpfx's default configuration file."
+  private val defaultLangDescription = "ftpfx's default language file."
+  private val configPath = Paths.get("rsc/conf/ftpfxDefault.conf")
+  private val langPath = Paths.get("rsc/lang/ftpfx-en.conf")
   private val config: java.util.Properties = loadConfig()
   //TODO needs to be defined
   private val language: java.util.Properties = loadLanguage()
@@ -14,18 +18,31 @@ object ConfigObj {
   private def loadConfig(): java.util.Properties = {
     val conf: java.util.Properties = new java.util.Properties()
     if (!Files.exists(configPath)) {
-      defaultConfKeys.foreach { case (key, value) => conf.setProperty(key, value) }
-      conf.store(Files.newOutputStream(configPath), defaultDescription)
-    } else conf.load(Files.newInputStream(configPath))
+      DefaultValues.defaultConfKeys.foreach { case (key, value) => conf.setProperty(key, value) }
+      conf.store(Files.newOutputStream(configPath), defaultConfDescription)
+    } else {
+      conf.load(Files.newInputStream(configPath))
+      //the loaded file doesn't contain all keys => use the default-keyset
+      if (!checkConfig(conf))
+        DefaultValues.defaultConfKeys.foreach { case (key, value) => conf.setProperty(key, value) }
+
+      //TODO if lanuage != en --> load -de.conf file
+    }
+
     return conf
   }
 
   private def loadLanguage(): java.util.Properties = {
     val prop: java.util.Properties = new java.util.Properties()
     if (!Files.exists(langPath)) {
-      defaultLangKeys.foreach { case (key, value) => prop.setProperty(key, value) }
-      prop.store(Files.newOutputStream(langPath), defaultDescription)
-    } else prop.load(Files.newInputStream(langPath))
+      DefaultValues.defaultLangKeys.foreach { case (key, value) => prop.setProperty(key, value) }
+      prop.store(Files.newOutputStream(langPath), defaultLangDescription)
+    } else {
+      prop.load(Files.newInputStream(langPath))
+      //the loaded file doesn't contain all keys => use the default-keyset
+      if (!checkLanguage(prop))
+        DefaultValues.defaultLangKeys.foreach { case (key, value) => prop.setProperty(key, value) }
+    }
 
     return prop
   }
@@ -45,11 +62,37 @@ object ConfigObj {
     case x: String => Some(x)
   }
 
-  //==================================================================
-  //default key-values, if the file doesn't exsts
-  private val defaultConfKeys: Map[String, String] = Map(
-    "config-file" -> "this",
-    "version" -> "1.0",
-    "language" -> "en")
-  private val defaultLangKeys: Map[String, String] = Map()
+  private def checkConfig(conf: java.util.Properties): Boolean = {
+    val origKeys = DefaultValues.defaultConfKeys.keySet
+    val extractedKeys = conf.keySet().filter(x => x.isInstanceOf[String])
+    return origKeys.forall { x => extractedKeys.contains(x) }
+  }
+
+  private def checkLanguage(prop: java.util.Properties): Boolean = {
+    val origKeys = DefaultValues.defaultLangKeys.keySet
+    val extractedKeys = prop.keySet().filter(x => x.isInstanceOf[String])
+    return origKeys.forall { x => extractedKeys.contains(x) }
+  }
 }
+
+//==================================================================
+//default key-values, if the file doesn't exsts
+private object DefaultValues {
+  /*
+   * The value this/default is used for default-values
+   */
+  val defaultConfKeys: Map[String, String] = Map(
+    "config-file" -> "this",
+    "language-file" -> "default",
+    "version" -> "1.0",
+    "language" -> "en",
+    "theme" -> "default")
+  val defaultLangKeys: Map[String, String] = Map(
+    "upload-btn-lbl" -> "Upload",
+    "download-btn-lbl" -> "Download",
+    "loads-tab-lbl" -> "Up-/Downloads",
+    "log-tab-lbl" -> "Log",
+    "file-menue-lbl" -> "File",
+    "help-menue-lbl" -> "Help")
+}
+//==================================================================
---
+++ FtpClient/tests/ftp/util/ConfigObjTest.scala
@@ -0,0 +1,24 @@
+package ftp.util
+
+import org.junit.Test
+import org.junit.Assert._
+import org.junit.runner.RunWith
+
+class ConfigObjTest {
+  @Test
+  def test() = {
+    assertEquals(Some("1.0"), ConfigObj.getC("version"))
+    assertEquals(Some("this"), ConfigObj.getC("config-file"))
+    assertEquals(Some("en"), ConfigObj.getC("language"))
+
+    assertEquals(None, ConfigObj.getC("blup"))
+    assertEquals(None, ConfigObj.getC("dat"))
+
+    assertEquals(Some("Upload"), ConfigObj.getL("upload-btn-lbl"))
+    assertEquals(Some("Log"), ConfigObj.getL("log-tab-lbl"))
+    assertEquals(Some("Up-/Downloads"), ConfigObj.getL("loads-tab-lbl"))
+
+    assertEquals(None, ConfigObj.getL("blup"))
+    assertEquals(None, ConfigObj.getL("dat"))
+  }
+}
